21ad72dc19ee1ae5773b802a6104046c1de01e7e
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-11-07 11:58:24 +0530

    Bug fixes

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 "use-strict";
 
+const player = document.querySelector(".player img");
+const enemy = document.querySelector(".opponent img");
+const message = document.querySelector(".message");
+const playerScore = document.querySelector(".player .score");
+const opponentScore = document.querySelector(".opponent .score");
+
+function resetGame() {
+    playerScore.textContent = 0;
+    opponentScore.textContent = 0;
+    player.src = `images/image1.webp`;
+    enemy.src = `images/image1.webp`;
+}
+
 function setImage(playerChoice, computerChoice) {
-    const player = document.querySelector(".player img");
-    const enemy = document.querySelector(".opponent img");
+
     player.src = `images/image${playerChoice}.webp`;
     enemy.src = `images/image${computerChoice}.webp`;
 }
 
 function computer_plays(playerChoice) {
-    const message = document.querySelector(".message");
     const computerMove = Math.trunc(Math.random() * 3 + 1);
-    const playerScore = document.querySelector(".player .score");
-    const opponentScore = document.querySelector(".opponent .score");
-
     let playerWins = true;
     let draw = false;
     setImage(playerChoice, computerMove);
@@ -21,14 +29,14 @@ function computer_plays(playerChoice) {
         draw = true;
     } else if (playerChoice === 1) {
         if (computerMove === 2) {
-            message.textContent = "You Lose";
+            message.textContent = "Computer Wins";
             playerWins = false;
         } else if (computerMove === 3) {
             message.textContent = "You Win";
         }
     } else if (playerChoice === 2) {
         if (computerMove === 3) {
-            message.textContent = "You Lose";
+            message.textContent = "Computer Wins";
             playerWins = false;
         } else if (computerMove === 1) {
             message.textContent = "You Win";
@@ -36,11 +44,12 @@ function computer_plays(playerChoice) {
     } else if (playerChoice === 3) {
         if (computerMove === 1) {
             playerWins = false;
-            message.textContent = "You Lose";
+            message.textContent = "Computer Wins";
         } else if (computerMove === 2) {
             message.textContent = "You Win";
         }
     }
+
 
     if (!draw) {
         if (playerWins) {
@@ -50,6 +59,7 @@ function computer_plays(playerChoice) {
         }
     }
 
+
 }
 
 window.addEventListener("click", (e) => {
@@ -58,3 +68,7 @@ window.addEventListener("click", (e) => {
         computer_plays(playerChoice);
     }
 })
+
+
+
+resetGame();
